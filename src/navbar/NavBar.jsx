8c385575fa3f1a0d1af257bfe3882042606c40a0
thengo1 <tbn2109@columbia.edu>
2021-07-21 05:01:31 -0400

    adding NavBar

---
+++ src/navbar/NavBar.jsx
@@ -0,0 +1,11 @@
+import './navbar.css'
+
+export default function NavBar() {
+    return (
+        <div className="nav">
+            <div className="topLeft">l</div>
+            <div className="topCenter">c</div>
+            <div className="topRight">r</div>
+        </div>
+    )
+}
